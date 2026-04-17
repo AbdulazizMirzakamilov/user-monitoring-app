daa6693d2da3c42074e7f8675e6f78afdc6c2e9f
Abdulaziz <alikom904@gmail.com>
2026-04-17 11:05:56 +0700

    feat: add UI for anomaly report and bind data to grid

--- UserMonitoringApp/MainWindow.xaml.cs
+++ UserMonitoringApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Npgsql;
 using System.Configuration;
+using UserMonitoringApp.Services;
 using System.Windows;
 
 namespace UserMonitoringApp
@@ -9,6 +10,19 @@ namespace UserMonitoringApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void LoadReport_Click(object sender, RoutedEventArgs e)
+        {
+            var service = new MonitoringService();
+
+            var data = service.GetAnomalyReport(
+                dateFrom.SelectedDate.Value,
+                dateTo.SelectedDate.Value,
+                int.Parse(thresholdBox.Text)
+            );
+
+            dataGrid.ItemsSource = data;
         }
     }
 }
